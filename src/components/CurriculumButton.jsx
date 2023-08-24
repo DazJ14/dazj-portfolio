09cb2cc15959a9ce0c55b679e7c3aa371cf28907
Diego Zamora <jaguares_diego@hotmail.com>
2023-08-23 21:26:57 -0600

    editing downloadcv button

--- src/components/CurriculumButton.jsx
+++ src/components/CurriculumButton.jsx
@@ -2,7 +2,7 @@
 const CurriculumButton = ({ languageSet, languageChange }) => {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=12VZBbO4bn_suHWSaadj3MUGT7LiQbL0N"
+      href="https://drive.google.com/uc?export=download&id=1EWd6OB3_801hZDu5nHf02jYsK03ABnre"
       download="curriculum.pdf"
       className="bg-sky-400 hover:bg-sky-500 py-2 px-4 rounded shadow-xl transition-colors"
     >
